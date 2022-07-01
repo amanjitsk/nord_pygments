9545cd3979d3e81c390cf8e539334f9d2d0191e4
Amanjit <amanjitsk@cs.toronto.edu>
2022-07-01 15:07:43 -0400

    add flattened; also make comments italic according to pygments guidelines

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup
 setup(
     name="nord_pygments",
     version="0.1",
-    py_modules=["nord_pygments", "ansi"],
+    py_modules=["nord_pygments", "ansi", "flattened"],
     license="BSD-3",
     entry_points={
         "pygments.styles": [
             "nord = nord_pygments:NordStyle",
             "terminal = ansi:AnsiStyle",
+            "flattened-light = flattened:FlattenedLightStyle",
+            "flattened-dark = flattened:FlattenedDarkStyle",
         ]
     },
 )
